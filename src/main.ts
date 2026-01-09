b8c52d98ff20578303bd88cb7bdcdeb16a9f75d4
Prasanjit Sarker <sarkerprasanjit379@gmail.com>
2026-01-09 14:29:00 +0600

    fix:main .ts file change

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
-
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // if you need cookies
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
